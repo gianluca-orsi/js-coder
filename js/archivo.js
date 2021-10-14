0b7df822b249f7b1ddf2470a874cc88ea3777f10
gianluca-orsi <orsigianluca01@gmail.com>
2021-10-14 08:30:01 -0300

    desafio 1

---
+++ js/archivo.js
@@ -0,0 +1,10 @@
+let nombre;
+let apellido;
+let numDocumento;
+
+
+nombre = prompt('Ingrese su nombre: ');
+apellido = prompt('Ingrese su apellido: ');
+numDocumento = prompt('Ingrese su numero de documento: ');
+
+alert('Hola ' + nombre + ' ' + apellido + ', tu numero de documento es ' + numDocumento + '.');
