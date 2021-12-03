2c1fecfcbcab7b6992d5b66f6fb54b9a7a7d5bb6
gianluca-orsi <orsigianluca01@gmail.com>
2021-12-03 17:31:19 -0300

    intentando usar ajax

---
+++ js/consultas.js
@@ -0,0 +1,63 @@
+const URLCONSULTAS = "../js/consultas.json"
+
+// con ajax podria hacer un formulario de consultas, cuando los datos estan bien se hace un popup afirmativo o negativo en caso de que falten datos
+function iniciarConsulta() {
+    let modalConsulta = $("#modalConsulta")
+    $(modalConsulta).addClass("is-active is-clipped")
+    if (localStorage.getItem("nombreUsuario") == '' || localStorage.getItem("nombreUsuario") == null ||
+        localStorage.getItem("apellidoUsuario") == '' || localStorage.getItem("apellidoUsuario") == null ||
+        localStorage.getItem("telefonoUsuario") == '' || localStorage.getItem("telefonoUsuario") == null ||
+        localStorage.getItem("domicilioUsuario") == '' || localStorage.getItem("domicilioUsuario") == null ||
+        localStorage.getItem("emailUsuario") == '' || localStorage.getItem("emailUsuario") == null)
+        {
+            $("#nombreUsuarioC").prop("disabled", false);
+            $("#apellidoUsuarioC").prop("disabled", false);
+            $("#telefonoUsuarioC").prop("disabled", false);
+            $("#domicilioUsuarioC").prop("disabled", false);
+            $("#emailUsuarioC").prop("disabled", false);
+        }
+    else {
+        $("#nombreUsuarioC").val(localStorage.getItem("nombreUsuario"));
+        $("#apellidoUsuarioC").val(localStorage.getItem("apellidoUsuario"));
+        $("#telefonoUsuarioC").val(localStorage.getItem("telefonoUsuario"));
+        $("#domicilioUsuarioC").val(localStorage.getItem("domicilioUsuario"));
+        $("#emailUsuarioC").val(localStorage.getItem("emailUsuario"));
+    }
+}
+
+function cerrarC () {
+    let modalConsulta = $("#modalConsulta");
+    $(modalConsulta).removeClass("is-active is-clipped");
+}
+
+function enviarConsulta() {
+    var INFOPOST = {
+        userName: $("#nombreUsuarioC").val(), 
+        userLastName: $("#apellidoUsuarioC").val(), 
+        phone: $("#telefonoUsuarioC").val(), 
+        address: $("#domicilioUsuarioC").val(), 
+        email: $("#emailUsuarioC").val()
+    }
+
+    $.ajax({
+        type: 'post',
+        url: './consultas.json',
+        data: JSON.stringify(INFOPOST),
+        contentType: 'application/json; charset=utf-8',
+        traditional: true,
+        success: $("#paraAgregarRespuesta").append(
+            `<div>
+                Gracias, tu consulta se ha enviado con exito.
+            </div>`
+        )
+    })
+    /* $.post (URLCONSULTAS, infoJSON, function (respuesta, estado) {
+        if (estado === "success") {
+            $("#botonEnviarConsulta").append(
+                `<div>
+                    ${respuesta.userName}, tu consulta se ha enviado con exito.
+                </div>`
+            )
+        }
+    }, 'json') */
+}
---
+++ js/usuario.js
@@ -0,0 +1,58 @@
+//hace aparecer un modal donde se puede cambiar el nombre de usuario --- falta
+function usuario () {
+    let menuUsuario = $("#modalUsuario")
+    $(menuUsuario).addClass("is-active is-clipped");
+    $("#nombreUsuario").val(localStorage.getItem("nombreUsuario"));
+    $("#apellidoUsuario").val(localStorage.getItem("apellidoUsuario"));
+    $("#telefonoUsuario").val(localStorage.getItem("telefonoUsuario"));
+    $("#domicilioUsuario").val(localStorage.getItem("domicilioUsuario"));
+    $("#emailUsuario").val(localStorage.getItem("emailUsuario"));
+}
+
+//saca el disabled a los inputs del modal usuario para poder ingresar nuevos datos
+function modificarDatos (){
+    $("#nombreUsuario").prop("disabled", false);
+    $("#apellidoUsuario").prop("disabled", false);
+    $("#telefonoUsuario").prop("disabled", false);
+    $("#domicilioUsuario").prop("disabled", false);
+    $("#emailUsuario").prop("disabled", false);
+}
+
+//cargar datos al modal usuario
+function cargarDatos () {
+    let nombreUsuario = $("#nombreUsuario").val();
+    let apellidoUsuario = $("#apellidoUsuario").val();
+    let telefonoUsuario = $("#telefonoUsuario").val();
+    let domicilioUsuario = $("#domicilioUsuario").val();
+    let emailUsuario = $("#emailUsuario").val();
+    localStorage.setItem("nombreUsuario", nombreUsuario);
+    localStorage.setItem("apellidoUsuario", apellidoUsuario);
+    localStorage.setItem("telefonoUsuario", telefonoUsuario);
+    localStorage.setItem("domicilioUsuario", domicilioUsuario);
+    localStorage.setItem("emailUsuario", emailUsuario);
+    $("#nombreUsuario").prop("disabled", true);
+    $("#apellidoUsuario").prop("disabled", true);
+    $("#telefonoUsuario").prop("disabled", true);
+    $("#domicilioUsuario").prop("disabled", true);
+    $("#emailUsuario").prop("disabled", true);
+}
+
+//
+function borrarDatos () {
+    localStorage.removeItem("nombreUsuario");
+    localStorage.removeItem("apellidoUsuario");
+    localStorage.removeItem("telefonoUsuario");
+    localStorage.removeItem("domicilioUsuario");
+    localStorage.removeItem("emailUsuario");
+    $("#nombreUsuario").val("");
+    $("#apellidoUsuario").val("");
+    $("#telefonoUsuario").val("");
+    $("#domicilioUsuario").val("");
+    $("#emailUsuario").val("");
+}
+
+//cierra el modal de usuario
+function cerrar () {
+    let modalUsuario = $("#modalUsuario");
+    $(modalUsuario).removeClass("is-active is-clipped");
+}
